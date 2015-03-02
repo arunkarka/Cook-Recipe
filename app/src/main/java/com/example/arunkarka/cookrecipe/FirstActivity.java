55082a12523e9cf18edad20da949e8a43ea7313d
arunkarka <arunkarka@gmail.com>
2015-03-01 18:57:33 -0600

    Removed List View in First Activity

--- app/src/main/java/com/example/arunkarka/cookrecipe/FirstActivity.java
+++ app/src/main/java/com/example/arunkarka/cookrecipe/FirstActivity.java
@@ -22,18 +22,6 @@ public class FirstActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        final ListView listview = (ListView) findViewById(R.id.listview);
-        String[] values = new String[] { "Tomotos", "Diced Onions", "Garlic",
-                "Salt", "Lime", "Salt", "Green Chilis", "Cilantro"};
-
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
-        listview.setAdapter(adapter);
-
 
         findViewById(R.id.begin_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,28 +60,4 @@ public class FirstActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-    }
 }
