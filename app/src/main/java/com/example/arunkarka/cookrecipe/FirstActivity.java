a8b9f2d2748bdb5af302a31920f19ee350b173bb
arunkarka <arunkarka@gmail.com>
2015-03-07 10:18:30 -0600

    Removed Action Bar

--- app/src/main/java/com/example/arunkarka/cookrecipe/FirstActivity.java
+++ app/src/main/java/com/example/arunkarka/cookrecipe/FirstActivity.java
@@ -1,5 +1,6 @@
 package com.example.arunkarka.cookrecipe;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
@@ -15,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class FirstActivity extends ActionBarActivity {
+public class FirstActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +31,6 @@ public class FirstActivity extends ActionBarActivity {
             }
         });
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_first, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void startRecipeActivity(){
--- app/src/main/java/com/example/arunkarka/cookrecipe/RecipeActivity.java
+++ app/src/main/java/com/example/arunkarka/cookrecipe/RecipeActivity.java
@@ -1,5 +1,6 @@
 package com.example.arunkarka.cookrecipe;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class RecipeActivity extends ActionBarActivity {
+public class RecipeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +30,6 @@ public class RecipeActivity extends ActionBarActivity {
                 startBonusActivity();
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_recipe, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void startBonusActivity(){
